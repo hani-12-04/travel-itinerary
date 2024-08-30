1f10a251b1843a9cad841946bd093f2b335a8f96
Hani Ali <haniali1204@gmail.com>
2024-08-30 11:34:16 +0100

    initial commit

---
+++ src/test/java/com/example/travel_itinerary/TravelItineraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travel_itinerary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelItineraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
